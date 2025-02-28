e59af60f167d568cf93e964f409ee13e2eb7a703
luangitdev <luaninveste@gmail.com>
2025-02-28 18:21:03 -0300

    feat: adicionar extensão SpringBootServletInitializer

--- src/main/java/com/example/olamundo/OlaMundoApplication.java
+++ src/main/java/com/example/olamundo/OlaMundoApplication.java
@@ -2,6 +2,7 @@ package com.example.olamundo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
