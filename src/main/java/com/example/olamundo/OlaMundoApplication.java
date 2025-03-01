b28f0b5850a63bfe9c8d7287e92a0d08460008a4
luangitdev <luaninveste@gmail.com>
2025-02-28 23:18:15 -0300

    feat: incrementar nível de logs no código da aplicação

--- src/main/java/com/example/olamundo/OlaMundoApplication.java
+++ src/main/java/com/example/olamundo/OlaMundoApplication.java
@@ -2,20 +2,32 @@ package com.example.olamundo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 @SpringBootApplication
 @Controller
-public class OlaMundoApplication {
+public class OlaMundoApplication extends SpringBootServletInitializer {
+    private static final Logger logger = LoggerFactory.getLogger(OlaMundoApplication.class);
 
     public static void main(String[] args) {
+        logger.info("Iniciando via main...");
         SpringApplication.run(OlaMundoApplication.class, args);
+    }
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        logger.info("Configurando Spring Boot para Tomcat externo...");
+        return builder.sources(OlaMundoApplication.class);
     }
 
     @GetMapping("/")
     public String home() {
-        return "index"; // Retorna o template index.html
+        logger.info("Endpoint / acessado");
+        return "index";
     }
 }
